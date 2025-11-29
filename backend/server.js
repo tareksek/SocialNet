8fcc5ca80fe6fe6f13c7aeac4743225b436267a1
tareksek <tareknegoudi@gmail.com>
2025-11-29 21:30:23 +0100

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// تحميل قاعدة البيانات
+global.db = JSON.parse(fs.readFileSync("./backend/database.json", "utf8"));
+
+// الراوتر
+app.use("/api/auth", authRoutes);
+
+// تشغيل السيرفر
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
